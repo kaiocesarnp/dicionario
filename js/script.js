86f5c7df99d7f8018905f6dcba407710bc774bc3
kaiocesarnp <kaiocesarnp@gmail.com>
2023-01-06 18:37:24 -0300

    dicionario

--- js/script.js
+++ js/script.js
@@ -4,40 +4,13 @@ let elementoCarregamento = document.querySelector(".js-carregamento");
 let elementoResultadoTitulo = document.querySelector(".js-resultado__titulo");
 let elementoResultadoDescricao = document.querySelector(".js-resultado__descricao");
 
-//https://api.dicionario-aberto.net/word/cavalo
 
-elementoFormulario.addEventListener("submit", (evento) => {
-    evento.preventDefault(); //impede de dar o refresh automaticamente
-
-    escondeMostraResultado(true);
-
-    let palavra = evento.target[0].value;
-    let url = `https://api.dicionario-aberto.net/word/cavalo${palavra}`
-
-    console.log(url);
-    //requisição
-    fetch(url).then((resposta) => resposta.json())
-    .then((resposta) => {
-        
-    if(!resposta[0]){ // !resposta[0] = esse exclamação+resp+0 significa  'se não houver palavra identificada' vem o alerta debaixo
-        erroPalavraNaoEcontrada ();
-        return;
-        }
-    let conteudoParseado = parseXML(resposta[0].xml);
-    inserirRespostas(conteudoParseado);
-    })
-
-    .finally(() => {
-        escondeMostraResultado();
-    });
-  });
-
-  function erroPalavraNaoEcontrada () { //busca o if !resposta[0]
+  function erroPalavraNaoEncontrada() { //busca o if !resposta[0]
     elementoResultadoTitulo.textContent = "Palavra não encontrada, verifique a grafia e tente novamente!";
         elementoResultadoDescricao.textContent = "";
   }
 
-  function escondeMostraResultado (mostrar = false){
+  function escondeMostraResultado(mostrar = false){
     if (mostrar){
         elementoResultado.classList.remove("display-none");
         elementoCarregamento.classList.remove("display-none"); //remove a classe display-none e faz com q o simbolo de carregamento apareça
@@ -69,3 +42,122 @@ elementoFormulario.addEventListener("submit", (evento) => {
     elementoResultadoDescricao.textContent = objRespostas.descricao
   }
 
+  function requisicaoFormulario(palavraParaBuscar){
+    let url = `https://api.dicionario-aberto.net/word/cavalo${palavraParaBuscar}`;
+
+    //requisição
+    fetch(url)
+        .then((resposta) => resposta.json())
+        .then((resposta) => {
+            if(!resposta[0]){ // !resposta[0] = esse exclamação+resp+0 significa  'se não houver palavra identificada' vem o alerta debaixo
+            erroPalavraNaoEncontrada();
+            return;
+        }
+
+    let conteudoParseado = parseXML(resposta[0].xml);
+            inserirRespostas(conteudoParseado);
+        })
+
+    .finally(() => {
+        escondeMostraResultado();
+    });
+  }
+
+  function gerenciarFormulario(){
+    elementoFormulario.addEventListener("submit", (evento) => {
+        evento.preventDefault(); //impede de dar o refresh automaticamente
+        escondeMostraResultado(true);
+        let palavra = evento.target[0].value;
+        requisicaoFormulario(palavra);
+    
+      });
+  }
+
+  gerenciarFormulario();
+
+
+
+
+
+
+
+
+
+//   let elementoFormulario = document.querySelector(".js-formulario");
+// let elementoResultado = document.querySelector(".js-resultado");
+// let elementoCarregamento = document.querySelector(".js-carregamento");
+// let elementoResultadoTitulo = document.querySelector(".js-resultado__titulo");
+// let elementoResultadoDescricao = document.querySelector(
+//   ".js-resultado__descricao"
+// );
+
+// function erroPalavraNaoEncontrada() {
+//   elementoResultadoTitulo.textContent =
+//     "Palavra não encontrada, verifique a grafia e tente novamente!";
+//   elementoResultadoDescricao.textContent = "";
+// }
+
+// function escondeMostraResultado(mostrar = false) {
+//   if (mostrar) {
+//     elementoResultado.classList.remove("display-none");
+//     elementoCarregamento.classList.remove("display-none");
+//   } else {
+//     elementoCarregamento.classList.add("display-none");
+//   }
+// }
+
+// function parserXML(data) {
+//   let resposta = {
+//     titulo: "",
+//     descricao: "",
+//   };
+//   funcaoDeParseamento = new DOMParser();
+
+//   resposta.titulo = funcaoDeParseamento
+//     .parseFromString(data, "text/xml")
+//     .getElementsByTagName("form")[0]
+//     .getElementsByTagName("orth")[0].textContent;
+
+//   resposta.descricao = funcaoDeParseamento
+//     .parseFromString(data, "text/xml")
+//     .getElementsByTagName("sense")[0]
+//     .getElementsByTagName("def")[0].textContent;
+
+//   return resposta;
+// }
+
+// function inserirRespostas(objRespostas) {
+//   elementoResultadoTitulo.textContent = objRespostas.titulo;
+//   elementoResultadoDescricao.textContent = objRespostas.descricao;
+// }
+
+// function requisicaoFormulario(palavraParaBuscar) {
+//   let url = `https://api.dicionario-aberto.net/word/${palavraParaBuscar}`;
+
+//   fetch(url)
+//     .then((resposta) => resposta.json())
+//     .then((resposta) => {
+//       if (!resposta[0]) {
+//         erroPalavraNaoEncontrada();
+//         return;
+//       }
+
+//       let conteudoParseado = parserXML(resposta[0].xml);
+//       inserirRespostas(conteudoParseado);
+//     })
+//     .finally(() => {
+//       escondeMostraResultado();
+//     });
+// }
+
+// function gerenciarFormulario() {
+//   elementoFormulario.addEventListener("submit", (evento) => {
+//     evento.preventDefault();
+
+//     escondeMostraResultado(true);
+//     let palavra = evento.target[0].value;
+//     requisicaoFormulario(palavra);
+//   });
+// }
+
+// gerenciarFormulario();
